f8793a6cceaa4c36f8766f71671523a0e1a47afc
stiwfmjX <57611278+stiwfmjX@users.noreply.github.com>
2019-12-27 19:28:09 +0800

    Add files via upload

---
+++ USOR.java
@@ -0,0 +1,27 @@
+package Class;
+
+public class USOR {
+	private int CMNO;//评论ID
+	private int UNO;//用户ID
+	private int USO;//是否有用，有用1，无用0，默认无用
+	
+	public int getCMNO() {
+		return CMNO;
+	}
+	public void setCMNO(int cMNO) {
+		CMNO = cMNO;
+	}
+	public int getUNO() {
+		return UNO;
+	}
+	public void setUNO(int uNO) {
+		UNO = uNO;
+	}
+	public int getUSO() {
+		return USO;
+	}
+	public void setUSO(int uSO) {
+		USO = uSO;
+	}
+	
+}
