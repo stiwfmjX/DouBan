9b21ac1cdef7e72fca35fb8b106891ea0ccdf356
stiwfmjX <57611278+stiwfmjX@users.noreply.github.com>
2019-12-27 19:27:04 +0800

    Add files via upload

---
+++ CMDAO.java
@@ -0,0 +1,12 @@
+package DAO;
+
+import java.util.List;
+
+import Class.CM;
+
+public interface CMDAO {
+	public void insertCM(CM cm);
+	//public void updateCM(CM cm);
+	public void deleteCM(int cmid);
+	public List<CM> getCMs(String mno);
+}
